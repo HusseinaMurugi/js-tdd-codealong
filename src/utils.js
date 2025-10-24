4a43089811c8ba240aa738c59adb60d1a073ae84
Husseina Murugi Shezad <hussxinamurugi@gmail.com>
2025-10-24 10:33:03 +0300

    added solutions

--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,6 @@
-// Your code here
+// src/utils.js
+
+// Function to calculate current age based on birth year
+export function currentAgeForBirthYear(birthYear) {
+  return new Date().getFullYear() - birthYear;
+}
